aa6620812509a0ffabaa6e122ccc5f847c0439ce
YunDaYoung <ydiosa98@gmail.com>
2020-09-10 06:15:03 +0900

    #6.0 Organize Admin Panel

--- rooms/admin.py
+++ rooms/admin.py
@@ -15,7 +15,77 @@ class RoomAdmin(admin.ModelAdmin):
 
     """ Room Admin definition """
 
-    pass
+    fieldsets = (  # filed 부제목 추가 및 재정렬
+        (
+            "Basic Info",
+            {"fields": ("name", "description", "country", "address", "price")},
+        ),
+        ("Times", {"fields": ("check_in", "check_out", "instant_book")}),
+        (
+            "Spaces",
+            {
+                "fields": (
+                    "guests",
+                    "beds",
+                    "bedrooms",
+                    "baths",
+                )
+            },
+        ),
+        (
+            "More About the Space",
+            {
+                "classes": ("collapse",),
+                "fields": (
+                    "amenities",
+                    "facilities",
+                    "house_rules",
+                ),
+            },
+        ),
+        (
+            "Last Details",
+            {"fields": ("host",)},
+        ),
+    )
+
+    list_display = (  # 목록 시 칼럼네임 지정
+        "name",
+        "country",
+        "city",
+        "price",
+        "guests",
+        "beds",
+        "bedrooms",
+        "baths",
+        "check_in",
+        "check_out",
+        "instant_book",
+        "count_amenities",
+    )
+
+    list_filter = (  # 필터기능 추가
+        "instant_book",
+        "host__superhost",
+        "room_type",
+        "amenities",
+        "facilities",
+        "house_rules",
+        "city",
+        "country",
+    )
+
+    search_fields = ["city", "^host__username"]  # 해당 filed에 관련된 검색가능
+
+    filter_horizontal = (  # ManyToManyField 만 가능
+        "amenities",
+        "facilities",
+        "house_rules",
+    )
+
+    def count_amenities(self, obj):  # obj는 객실
+        print(obj.amenities.all())
+        return "Potato"
 
 
 @admin.register(models.Photo)
