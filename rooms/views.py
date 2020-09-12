006830cc9d1fe3be59641bc7c0e6d0850167cd26
YunDaYoung <ydiosa98@gmail.com>
2020-09-12 17:53:27 +0900

    Introduction to Views and URLS

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from . import models
 
-# Create your views here.
+# from datetime import datetime
+# from django.http import HttpResponse
+
+
+def all_rooms(request):  # request를 받았으면 HttpRespense로 응답하는 구조
+    all_rooms = models.Room.objects.all()
+    return render(
+        request,
+        "rooms/home.html",
+        context={"rooms": all_rooms},
+    )
